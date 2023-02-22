452bb655c6c73e5f628c804c7f60ec1092e15ba1
MoAyoub <medayb45esi@gmail.com>
2023-02-22 23:08:41 +0100

    add pub & twln

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,14 @@ import express from "express";
 import * as http from "http";
 import  {Server} from "socket.io";
 import {ClientToServerEvents,InterServerEvents,ServerToClientEvents,SocketData} from "./types/socket";
-import {__dirname} from './utls/dirname';
+import {URL} from "url";
+
+
+//TODO Utls Functions
+const getDirRootName = ():String => {
+    return new URL('.', import.meta.url).pathname;
+}
+
 // Create Server
 
 const app = express();
@@ -10,7 +17,9 @@ const server = http.createServer(app)
 
 // Apply middleware
 app.use(express.json());
-//app.use(express.static())
+app.set("views",getDirRootName()+"views");
+app.set('view engine','pug')
+app.use(express.static(getDirRootName() + "public"))
 const io = new Server<
     ClientToServerEvents,
     ServerToClientEvents,
@@ -29,5 +38,9 @@ const PORT  = 3000 || process.env.PORT
 
 server.listen(PORT , () => {
     console.log('SERVER RUNNING ON PORT ', PORT);
-    console.log(__dirname)
 })
+
+
+
+
+
